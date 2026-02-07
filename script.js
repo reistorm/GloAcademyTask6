0d39e250841fd5e3f0cc1b7da7525b45e9a819a2
Eliza X. <rysaeva96@mail.ru>
2026-02-07 18:47:44 +0300

    выполнен hard-lesson06

--- script.js
+++ script.js
@@ -4,30 +4,38 @@ let number;
 
 const game = function (number) {
     const givenNumber = Math.floor(Math.random() * 100) + 1;
+    let attempt = 10;
     const asking = function () {
         const number = prompt('Введите число от 1 до 100');
+        const userNumber = parseInt(number, 10)
+        attempt = attempt - 1;
+        
         if (number === null) {
             alert("Игра окончена");
             return
         }
-
-        const userNumber = parseInt(number, 10)
+        if (attempt <= 0 && userNumber !== givenNumber) {
+            alert("Попытки закончились, хотите сыграть еще?")
+            return game();
+        }
         if (isNaN(userNumber)) {
             alert("Введите число")
             asking();
         } else {
             if (userNumber > givenNumber) {
-                alert("Загаданное число меньше");
+                alert(`Загаданное число меньше, осталось попыток ${attempt}`);
                 asking();
             } else if (userNumber < givenNumber) {
-                alert("Загаданное число больше");
+                alert(`Загаданное число больше, осталось попыток ${attempt}`);
                 asking();
             } else {
-                alert("Поздравляю, Вы угадали!!!");
+                alert("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+                return game();
             }
         }
-    };
-    asking()
+    }
+    asking();
 }
-
 game();
+
+
